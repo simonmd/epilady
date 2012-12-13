02ea9236a56d354458f33011921464cb6a7d716b
Simon Rascovsky <simonmd@gmail.com>
2012-12-13 14:02:57 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Epilady::Application.config.secret_token = '5de088d80a5932c26cf52a1f878d432a0ed6a37c2b4c36621268e342633e85a2b3346b3c1ab4395be578ac1c7a4ac252d25fbf08a59d43f0f28b4d26b58aac1e'
